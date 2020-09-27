ecbf02ecfcf677feab1f34b4e9fb67a755711be5
LoganTann <logon313@hotmail.fr>
2020-09-27 23:56:43 +0200

    Basic alpha Todo : fix special characters with out files

---
+++ sprint.c
@@ -0,0 +1,254 @@
+/*
+	sprint1.c
+	
+*/
+
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+typedef enum { FAUX = 0, VRAI = 1 } Booleen;
+Booleen EchoActif = FAUX;
+
+// Messages emis par les instructions
+
+#define MSG_DEVELOPPE "## nouvelle specialite \"%s\" ; cout horaire \"%d\"\n" 
+#define MSG_SPECIALITES "## consultation des specialites\n" 
+#define MSG_INTERRUPTION "## fin de programme\n"
+#define MSG_EMBAUCHE "## nouveau travailleur \"%s\" competent pour la specialite \"%s\"\n" 
+#define MSG_TRAVAILLEURS "## consultation des travailleurs competents pour la specialite \"%s\"\n"
+#define MSG_DEMARCHE "## nouveau client \"%s\"\n"
+#define MSG_CLIENT "## consultation des commandes effectuees par \"%s\"\n"
+#define MSG_COMMANDE "## nouvelle commande \"%s\", par client \"%s\"\n"
+#define MSG_SUPERVISION "## consultation de l’avancement des commandes\n"
+#define MSG_TACHE "## la commande \"%s\" requiere la specialite \"%s\" (nombre d’heures \"%d\")\n"
+#define MSG_CHARGE   "## consultation de la charge de travail de \"%s\"\n"
+#define MSG_PROGRESSION "## pour la commande \"%s\", pour la specialite \"%s\" : \"%d\" heures de plus ont ete realisees\n"
+
+// Lexemes
+
+#define LGMOT 35
+#define NBCHIFFREMAX 5 
+
+typedef char Mot[LGMOT + 1]; // Définition du type Mot
+
+/**
+	void get_id
+	@param id Mot: 
+**/
+void get_id(Mot id) {
+	scanf("%s", id);
+	if (EchoActif) printf(">>echo %s\n", id);
+}
+
+/**
+	int get_int()
+	get the current word from the input, and returns it with the (int) type
+**/
+int get_int() {
+	char buffer[NBCHIFFREMAX + 1];
+	scanf("%s", buffer);
+	if (EchoActif) printf(">>echo %s\n", buffer);
+	return atoi(buffer);
+}
+
+// Commandes
+
+/* 
+* traite_developpe()
+* developpe <Mot nom_specialite> <int cout_horaire>
+* Ajoute une specialité au programme
+*/
+void traite_developpe() {
+	Mot nom_specialite;
+	get_id(nom_specialite);
+	int cout_horaire = get_int();
+	printf(MSG_DEVELOPPE, nom_specialite, cout_horaire);
+}
+
+/* 
+* traite_specialites()
+* specialites
+* Affiche les specialités enregistrées dans le programme
+*/ 
+void traite_specialites() {
+	printf(MSG_SPECIALITES);
+}
+
+/*
+* traite_embauche() : traite les arguments de la commande suivante : 
+* embauche <Mot travailleur> <Mot specialite>
+*/
+void traite_embauche() {
+	Mot travailleur, specialite;
+	get_id(travailleur);
+	get_id(specialite);
+	printf(MSG_EMBAUCHE, travailleur, specialite);
+}
+
+/* 
+* traite_travailleurs()
+* travailleurs <Mot specialite = "tous">
+* Affiche les travailleurs selon la specialité choisie ou non
+*/ 
+void traite_travailleurs() {
+	Mot specialite;
+	get_id(specialite);
+	printf(MSG_TRAVAILLEURS, specialite);
+}
+
+/* 
+* traite_demarche()
+* demarche <Mot nom_client>
+* Ajoute un client au programme
+*/ 
+void traite_demarche() {
+	Mot nom_client;
+	get_id(nom_client);
+	printf(MSG_DEMARCHE, nom_client);
+}
+
+
+/* 
+* traite_client()
+* client <Mot nom_client>
+* Affiche les commandes d'un client
+*/ 
+void traite_client() {
+	Mot nom_client;
+	get_id(nom_client);
+	printf(MSG_CLIENT, nom_client);
+}
+
+/* 
+* traite_commande()
+* commande <Mot produit> <Mot nom_client>
+* Ajoute une commande au programme
+*/ 
+void traite_commande() {
+	Mot produit, nom_client;
+	get_id(produit);
+	get_id(nom_client);
+	printf(MSG_COMMANDE, produit, nom_client);
+}
+
+/*
+* traite_supervision()
+* supervision
+* Affiche l'avancement actuel des commandes
+*/
+void traite_supervision() {
+	printf(MSG_SUPERVISION);
+}
+
+/* 
+* traite_tache()
+* tache <Mot produit> <Mot specialite> <int heures>
+* Ajoute une specialité ainsi que le nombre d'heures requises à une commande
+*/ 
+void traite_tache() {
+	Mot produit, nom_client;
+	get_id(produit);
+	get_id(nom_client);
+	int heures = get_int();
+	printf(MSG_TACHE, produit, nom_client, heures);
+}
+
+/*
+* traite_charge()
+* charge <Mot nom_travailleur>
+* A
+*/
+void traite_charge() {
+	Mot nom_travailleur;
+	get_id(nom_travailleur);
+	printf(MSG_CHARGE, nom_travailleur);
+}
+
+/* 
+* traite_progression()
+* tache <Mot produit> <Mot specialite> <int heures_travaillees>
+* Enregistre une progression aux seins d'une specialité d'une commande
+*/ 
+void traite_progression() {
+	Mot produit, nom_client;
+	get_id(produit);
+	get_id(nom_client);
+	int heures_travaillees = get_int();
+	printf(MSG_PROGRESSION, produit, nom_client, heures_travaillees);
+}
+
+/* 
+* traite_interruption()
+* interruption
+* Interromp le programme
+*/ 
+void traite_interruption() {
+	printf(MSG_INTERRUPTION);
+}
+
+// Porte d'entrée du programme 
+
+int main(int argc, char* argv[]) {
+	if (argc >= 2 && strcmp("echo", argv[1]) == 0) {
+		EchoActif = VRAI;
+	}
+	setlocale(LC_ALL, "fr-FR");
+	Mot buffer;
+	while (VRAI) {
+		get_id(buffer);
+		if (strcmp(buffer, "developpe") == 0) {
+			traite_developpe();
+			continue;
+		}
+		if (strcmp(buffer, "specialites") == 0) {
+			traite_specialites();
+			continue;
+		}
+		if (strcmp(buffer, "embauche") == 0) {
+			traite_embauche();
+			continue;
+		}
+		if (strcmp(buffer, "travailleurs") == 0) {
+			traite_travailleurs();
+			continue;
+		}
+		if (strcmp(buffer, "demarche") == 0) {
+			traite_demarche();
+			continue;
+		}
+		if (strcmp(buffer, "client") == 0) {
+			traite_client();
+			continue;
+		}
+		if (strcmp(buffer, "commande") == 0) {
+			traite_commande();
+			continue;
+		}
+		if (strcmp(buffer, "supervision") == 0) {
+			traite_supervision();
+			continue;
+		}
+		if (strcmp(buffer, "tache") == 0) {
+			traite_tache();
+			continue;
+		}
+		if (strcmp(buffer, "charge") == 0) {
+			traite_charge();
+			continue;
+		}
+		if (strcmp(buffer, "progression") == 0) {
+			traite_progression();
+			continue;
+		}
+		if (strcmp(buffer, "interruption") == 0) {
+			traite_interruption();
+			break;
+		}
+
+		printf("!!! instruction inconnue >%s< !!!\n", buffer);
+	}
+	return 0;
+}
